0f059e9ec5713faf3ade896c2f48ea42a88145be
BARIŞ DEVELİ <62984011+barisdvl@users.noreply.github.com>
2021-12-06 22:19:16 +0300

    Add files via upload

---
+++ areaOfCircle.js
@@ -0,0 +1,8 @@
+const val = process.argv;
+
+function areaOfCircle(radius) {
+    let area = Math.PI * (Math.pow(radius,2));
+    console.log(`Yarıçapı ${radius} olan dairenin alanı: ${area}`)
+}
+
+areaOfCircle(val[2])
